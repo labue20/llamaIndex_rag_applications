51023a2a2d66cbe678e44174e4666299b4510ade
labue20 <labuewilfred@gmail.com>
2025-07-24 00:48:42 -0500

    Update App.js

--- llama_index_web_app/rag-web-app/src/App.js
+++ llama_index_web_app/rag-web-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import DocumentTools from './components/DocumentTools';
+import IndexQuery from './components/IndexQuery';
+import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <div className='content'>
+        <DocumentTools />
+        <IndexQuery />
+      </div>
     </div>
   );
 }
